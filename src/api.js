998f6b61a2144435f2ca77c12871a533636a947d
Egor Loktev <loktev@lok-tech.com>
2024-08-16 00:06:59 +0300

    fix api url

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
-  // baseURL: 'https://api.secret-talks.app',
+  // baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://api.secret-talks.app',
 });
 
 export default api;
